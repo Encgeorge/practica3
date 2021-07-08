6c17974b1a571d383e653acf4129e71d2a1d54e5
Encgeorge <enciugeorge99@gmail.com>
2021-07-08 11:49:37 +0300

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyAi7BkSSehWZb6JtMEditR1ht4rKO49VPw",
+    authDomain: "online-store-903c5.firebaseapp.com",
+    projectId: "online-store-903c5",
+    storageBucket: "online-store-903c5.appspot.com",
+    messagingSenderId: "243482837658",
+    appId: "1:243482837658:web:eb2e3f5b0806c2000bd523"
+  };
+  
